02bab77cbb7b4ab2d625233ef96ecf3c1f7e03e6
kdrkdrkdr <kdrhacker1234@gmail.com>
2023-10-14 01:03:25 +0900

    edit push_files.sh

--- push_files.sh
+++ push_files.sh
@@ -1,11 +1,14 @@
-app="com.samsung.android.ptts.kokr"
-wav_path="00010.wav"
-wav_content="$(cat $wav_path)"
+# 24hHz, mono, s16
+data_dir="datasets/hutao"
 
+app="com.samsung.android.ptts.kokr"
 mv_dir="$(adb shell run-as $app find /data/user/0/$app/ -name wavs_raw)"
 mv_file=$mv_dir/$wav_path
 
-
-# 파일 푸쉬 하는 것만 해결하면 됨.
-adb push $wav_path /sdcard/$wav_path
-adb -d shell "run-as $app echo > /sdcard/2.wav"
+for ((i=1; i<=18; i++)); do
+    filename=$(printf "%05d.wav" "$i")
+    wav_path=$data_dir/$filename
+    tmp_file="/data/local/tmp/$filename"
+    adb push $filename $tmp_file
+    adb shell run-as $app mv $tmp_file $mv_dir
+done
