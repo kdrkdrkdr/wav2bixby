fb6df5cc65bba531e8f957599ae60bd6a36bf916
kdrkdrkdr <kdrhacker1234@gmail.com>
2023-10-13 23:13:26 +0900

    edit app_signing

--- app_signing.sh
+++ app_signing.sh
@@ -1,7 +1,9 @@
+
 rm original.apk patched.apk patched.apk.idsig resign.keystore
-apk_location=$(adb shell pm list packages -f -3 | grep com.samsung.android.ptts.kokr | sed 's/.*:\(.*\).apk.*/\1/').apk
+apk_name=com.samsung.android.ptts.kokr
+apk_location=$(adb shell pm list packages -f -3 | grep $apk_name | sed 's/.*:\(.*\).apk.*/\1/').apk
 adb pull $apk_location original.apk
 keytool -genkey -v -keystore resign.keystore -storepass androiddbg -alias androiddbg -dname "CN=Android Debug,O=Android,C=US"
 python make_debuggable.py apk original.apk patched.apk resign.keystore androiddbg
-adb uninstall com.samsung.android.ptts.kokr
+adb uninstall $apk_name
 adb install patched.apk
